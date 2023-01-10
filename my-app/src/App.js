56574dc6677408f13dd5291766651491910ea242
Morning <morning@admins-iMac.local>
2023-01-10 12:13:19 +0800

    df

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,29 @@
 import "./App.css";
+import Header from "./component/Header";
 import { useState } from "react";
 
-function App() {
-  let [x, setX] = useState(100);
-  function decrement() {
-    setX(x - 1);
-  }
+// function App() {
+//   let [x, setX] = useState(100);
+//   function decrement() {
+//     setX(x - 1);
+//   }
 
-  function add() {
-    setX(x + 1);
-  }
+//   function add() {
+//     setX(x + 1);
+//   }
+//   return (
+//     <div className="App">
+//       <button onClick={decrement}>-</button>
+//       {x}
+//       <button onClick={add}>+</button>
+//     </div>
+//   );
+// }
+
+function App() {
   return (
     <div className="App">
-      <button onClick={decrement}>-</button>
-      {x}
-      <button onClick={add}>+</button>
+      <Header />
     </div>
   );
 }
---
+++ my-app/src/component/Header.jsx
@@ -0,0 +1,15 @@
+import "../style/header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h4>Total Players: 0</h4>
+      </div>
+      <div>
+        <button>ByName</button>
+        <button>ByScore</button>
+      </div>
+    </header>
+  );
+}
